0876d58c0d59fa3a4423be26b2682cef6ce8db70
Draven Grondona <dgrondona@ucmerced.edu>
2024-12-08 01:25:48 +0000

    added isLess function that detects whether state a is less than state b lexicographically

--- Permutate.h
+++ Permutate.h
@@ -2,62 +2,86 @@
 
 #include "GameState.h"
 
-// rotates the board by 90 degrees
-GameState rotate(GameState game) {
+class Permutate {
 
-    int n = game.size;
-    GameState rotated;
+    // rotates the board by 90 degrees
+    GameState rotate(GameState game) {
 
-    for (int i = 0; i < n; i++) {
+        int n = game.size;
+        GameState rotated;
 
-        for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++) {
 
-            rotated.grid[j][n - 1 - i] = game.grid[i][j];
+            for (int j = 0; j < n; j++) {
 
-        }
+                rotated.grid[j][n - 1 - i] = game.grid[i][j];
 
-    }
-
-    return rotated;
-
-}
-
-// reflects the board accross the horizontal
-GameState reflectHorizontal(GameState game) {
-
-    int n = game.size;
-    GameState reflected;
-
-    for (int i = 0; i < n/2; i++) {
-
-        for (int j = 0; j < n; j++) {
-
-            reflected.grid[i][j] = game.grid[n - 1 - i][j];
+            }
 
         }
 
+        return rotated;
+
     }
 
-    return reflected;
+    // reflects the board accross the horizontal
+    GameState reflectHorizontal(GameState game) {
 
-}
+        int n = game.size;
+        GameState reflected;
 
-// reflects the board accross the vertical
-GameState reflectVertical(GameState game) {
+        for (int i = 0; i < n/2; i++) {
 
-    int n = game.size;
-    GameState reflected;
+            for (int j = 0; j < n; j++) {
 
-    for (int i = 0; i < n; i++) {
+                reflected.grid[i][j] = game.grid[n - 1 - i][j];
 
-        for (int j = 0; j < n/2; j++) {
-
-            reflected.grid[i][j] = game.grid[i][n - 1 - j];
+            }
 
         }
 
+        return reflected;
+
     }
 
-    return reflected;
+    // reflects the board accross the vertical
+    GameState reflectVertical(GameState game) {
 
-}
+        int n = game.size;
+        GameState reflected;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n/2; j++) {
+
+                reflected.grid[i][j] = game.grid[i][n - 1 - j];
+
+            }
+
+        }
+
+        return reflected;
+
+    }
+
+    // determine if the value of game state a is less than game state b
+    bool isLess(GameState a, GameState b) {
+
+        int n = a.size; // get the size of the game states. a.size and b.size should just be the same
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                if (a.grid[i][j] < b.grid[i][j]) return true; // return true if a is less than b
+
+
+            }
+
+        }
+
+        return false; // otherwise return false
+
+    }
+
+};
