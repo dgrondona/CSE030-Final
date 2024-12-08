4e57012a6f8c6cc30a6831ebfc49a0db6c1c6cdd
Draven Grondona <dgrondona@ucmerced.edu>
2024-12-08 00:27:45 +0000

    added function to reflect the grid about the horizontal

--- Permutate.h
+++ Permutate.h
@@ -2,6 +2,7 @@
 
 #include "GameState.h"
 
+// rotates the board by 90 degrees
 GameState rotate(GameState game) {
 
     int n = game.size;
@@ -18,5 +19,25 @@ GameState rotate(GameState game) {
     }
 
     return rotated;
+
+}
+
+// reflects the board accross the horizontal
+GameState reflectHorizontal(GameState game) {
+
+    int n = game.size;
+    GameState reflected;
+
+    for (int i = 0; i < n/2; i++) {
+
+        for (int j = 0; j < n; j++) {
+
+            reflected.grid[i][j] = game.grid[n - 1 - i][j];
+
+        }
+
+    }
+
+    return reflected;
 
 }
